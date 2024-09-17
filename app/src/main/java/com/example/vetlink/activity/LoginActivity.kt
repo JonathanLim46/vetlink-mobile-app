25d3a11ddca52b6d6f5d3452a626c3aa028b3834
Kevin Farhan H <kevinfarhan95@gmail.com>
2024-09-17 18:54:58 +0700

    making base folder structer

--- app/src/main/java/com/example/vetlink/activity/LoginActivity.kt
+++ app/src/main/java/com/example/vetlink/activity/LoginActivity.kt
@@ -1,10 +1,11 @@
-package com.example.vetlink
+package com.example.vetlink.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vetlink.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/vetlink/activity/SecondSplashActivity.kt
+++ app/src/main/java/com/example/vetlink/activity/SecondSplashActivity.kt
@@ -1,5 +1,6 @@
-package com.example.vetlink
+package com.example.vetlink.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -7,18 +8,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vetlink.R
 
-class MainActivity : AppCompatActivity() {
-
+class SecondSplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SecondSplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
 
     }
 }
