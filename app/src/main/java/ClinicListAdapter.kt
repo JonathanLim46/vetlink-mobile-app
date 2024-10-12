ac8409a830f26fe65f068757fe967de03621b7d2
JonathanLim46 <jonathanfrizy12@gmail.com>
2024-10-13 01:13:17 +0700

    feat: assets, layout, home layout, data clinic list

---
+++ app/src/main/java/ClinicListAdapter.kt
@@ -0,0 +1,35 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vetlink.R
+
+class ClinicListAdapter(private val clinicList : List<ClinicList>) :
+    RecyclerView.Adapter<ClinicListAdapter.ClinicViewHolder>() {
+
+    class ClinicViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val clinicImageView : ImageView = itemView.findViewById(R.id.ivClinic)
+        val clinicNameTv : TextView = itemView.findViewById(R.id.tvCardClinicName)
+        val clinicLocationTv : TextView = itemView.findViewById(R.id.tvClinicLocation)
+        val clinicTimeOpenTv : TextView = itemView.findViewById(R.id.tvClinicTimeOpen)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClinicViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.each_item_clinic,parent,false)
+        return ClinicViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return clinicList.size
+    }
+
+    override fun onBindViewHolder(holder: ClinicViewHolder, position: Int) {
+        val clinicList = clinicList[position]
+        holder.clinicImageView.setImageResource(clinicList.clinicImage)
+        holder.clinicNameTv.text = clinicList.clinicName
+        holder.clinicLocationTv.text = clinicList.clinicLocation
+        holder.clinicTimeOpenTv.text = clinicList.clinicTimeOpen
+    }
+}
--- app/src/main/java/com/example/vetlink/activity/MainActivity.kt
+++ app/src/main/java/com/example/vetlink/activity/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.vetlink.activity
 
+import ClinicList
+import ClinicListAdapter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.vetlink.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var clinicList: ArrayList<ClinicList>
+    private lateinit var clinicListAdapter: ClinicListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        init()
+
+    }
+
+    private fun init() {
+        recyclerView = findViewById(R.id.rvClinicList)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+
+        clinicList = ArrayList()
+
+        addDataToList()
+
+        clinicListAdapter = ClinicListAdapter((clinicList))
+        recyclerView.adapter = clinicListAdapter
+    }
+
+    private fun addDataToList(){
+        clinicList.add(ClinicList(R.drawable.rspets, "Klinik IPB", "Sukmajaya, Depok", "Buka | 07.00 - 15.00"))
+        clinicList.add(ClinicList(R.drawable.rspets, "Klinik IPB", "Sukmajaya, Depok", "Buka | 07.00 - 15.00"))
+        clinicList.add(ClinicList(R.drawable.rspets, "Klinik IPB", "Sukmajaya, Depok", "Buka | 07.00 - 15.00"))
+        clinicList.add(ClinicList(R.drawable.rspets, "Klinik IPB", "Sukmajaya, Depok", "Buka | 07.00 - 15.00"))
     }
 }
