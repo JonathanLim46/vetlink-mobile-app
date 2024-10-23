53340280bda294479f42902aaa3a37f78f3947ef
JonathanLim46 <jonathanfrizy12@gmail.com>
2024-10-23 11:32:41 +0700

    com

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,19 +65,21 @@ dependencies {
 
     //retrofit library
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
     //gson converter library
-    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation ("com.google.code.gson:gson:2.11.0")
+//    implementation ("com.google.code.gson:gson:2.11.0")
 
     //okhttp library
     implementation ("com.squareup.okhttp3:okhttp:4.12.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
     //coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
 
     //ViewModel and LiveData
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+//    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
 }
