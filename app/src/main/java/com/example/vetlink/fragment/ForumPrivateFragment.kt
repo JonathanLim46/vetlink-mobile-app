0e26ecf35dd2c8e8ec00e3495997c2c05f169d8b
JonathanLim46 <jonathanfrizy12@gmail.com>
2024-11-02 12:48:33 +0700

    Jonathan Lim/feat: add my board view

--- app/src/main/java/com/example/vetlink/fragment/ForumPrivateFragment.kt
+++ app/src/main/java/com/example/vetlink/fragment/ForumPrivateFragment.kt
@@ -1,11 +1,18 @@
 package com.example.vetlink.fragment
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 import com.example.vetlink.R
+import com.example.vetlink.databinding.FragmentForumPrivateBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +28,12 @@ class ForumPrivateFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentForumPrivateBinding
+
+    val mainButtonBackground = R.drawable.layout_button_main
+    val mainTextColor = Color.parseColor("#FFFFFF")
+    val secondButtonBackground = R.drawable.layout_button_second
+    val secondTextColor = Color.parseColor("#B08BBB")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +48,47 @@ class ForumPrivateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forum_private, container, false)
+        binding = FragmentForumPrivateBinding.inflate(inflater, container, false)
+
+        initView()
+
+        return binding.root
+    }
+
+
+    private fun initView(){
+
+        with(binding){
+
+
+
+            btnAll.setBackgroundResource(mainButtonBackground)
+            btnAll.setTextColor(mainTextColor)
+
+            btnAll.setOnClickListener{
+                buttonState(btnAll, listOf(btnDone, btnInProgress))
+            }
+
+            btnDone.setOnClickListener{
+                buttonState(btnDone, listOf(btnAll, btnInProgress))
+            }
+
+            btnInProgress.setOnClickListener{
+                buttonState(btnInProgress, listOf(btnAll, btnDone))
+            }
+
+        }
+
+    }
+
+    private fun buttonState(selectedButton: Button, otherButton: List<Button>){
+        selectedButton.setBackgroundResource(mainButtonBackground)
+        selectedButton.setTextColor(mainTextColor)
+
+        for (button in otherButton){
+            button.setBackgroundResource(secondButtonBackground)
+            button.setTextColor(secondTextColor)
+        }
     }
 
     companion object {
