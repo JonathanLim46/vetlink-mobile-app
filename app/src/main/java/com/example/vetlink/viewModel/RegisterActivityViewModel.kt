af6760269151a2538f1e15fce7c1915afc5e72d1
Kevin Farhan H <kevinfarhan95@gmail.com>
2024-11-19 09:52:16 +0700

    kevin, features/check-email-and-username

--- app/src/main/java/com/example/vetlink/viewModel/RegisterActivityViewModel.kt
+++ app/src/main/java/com/example/vetlink/viewModel/RegisterActivityViewModel.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.vetlink.data.model.auth.LoginResponse
 import com.example.vetlink.data.model.auth.RegisterResponse
+import com.example.vetlink.data.network.response.auth.CheckResponse
 import com.example.vetlink.repository.AuthRepository
 import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
@@ -13,8 +13,45 @@ import java.net.ConnectException
 
 class RegisterActivityViewModel(private val repository: AuthRepository) : ViewModel() {
 
+
     val registerResponse = MutableLiveData<RegisterResponse?>()
     val errorMessage = MutableLiveData<String>()
+    val checkEmailResponse = MutableLiveData<CheckResponse>()
+    val checkUsernameResponse = MutableLiveData<CheckResponse>()
+    val checkPhoneResponse = MutableLiveData<CheckResponse>()
+
+    fun checkEmail(email: String){
+        viewModelScope.launch {
+            try {
+                val response = repository.checkEmail(email)
+                if (response.status == 200) { // Assuming response is a retrofit Response type
+                    checkEmailResponse.postValue(response)
+                } else {
+                    errorMessage.postValue("Registration failed: ${response.message}")
+                }
+            }catch (e: Exception){
+                errorMessage.postValue("An error occurred. Please try again.")
+                Log.e("API_ERROR", "Registration error: ${e.message}")
+            }
+        }
+    }
+
+    fun checkUsername(username: String){
+        viewModelScope.launch {
+            try {
+                val response = repository.checkUsername(username)
+                if (response.status == 200) { // Assuming response is a retrofit Response type
+                    checkUsernameResponse.postValue(response)
+                } else {
+                    errorMessage.postValue("Registration failed: ${response.message}")
+                }
+            }catch (e: Exception){
+                errorMessage.postValue("An error occurred. Please try again.")
+                Log.e("API_ERROR", "Registration error: ${e.message}")
+            }
+
+        }
+    }
 
     fun registerUser(
         name: String,
